09eb4da132ac12861fe197919ff24d1b1ca04326
juanse <jsalvarado405misena.edu.co>
2018-08-14 11:40:37 -0600

    TerminadaSinFuncionar

--- app/src/main/java/com/example/fhjua/webservicesmaps/MainActivity.java
+++ app/src/main/java/com/example/fhjua/webservicesmaps/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.fhjua.webservicesmaps;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -20,13 +31,18 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private EditText txtLatitud;
     private EditText txtLongitud;
     private TextView viewResult;
     private Button btnDatos;
+    private LocationManager locationManager;
+    private Location location;
+    private ObtenerWebService hiloConexion;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,16 +52,131 @@ public class MainActivity extends AppCompatActivity {
         txtLatitud = (EditText) findViewById(R.id.txt_latitud);
         txtLongitud = (EditText) findViewById(R.id.txt_longitud);
         viewResult = (TextView) findViewById(R.id.view_result);
-        btnDatos = (Button) findViewById(R.id.btn_envia_datos);
+        btnDatos = (Button) findViewById(R.id.btn_envia);
 
-        btnDatos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ObtenerWebService hiloConexion = new ObtenerWebService();
-                //Mandamos los parámetros que se nesecitan
-                hiloConexion.execute(txtLatitud.getText().toString(), txtLongitud.getText().toString());
+        /**
+         * Tenemos un objeto de la clase LocationManajer y a este le asignamos un
+         * servicio de localizacion
+         */
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        /**
+         * Valida si la version de la API en la cual se esta trabajando es mayor o igual a la API de nivel 23
+         * Si la condicion se cumple se le pide al usuario los permisos necesarios
+         */
+        int sdk = Build.VERSION.SDK_INT;
+        //Si la version del SDK del dispositivo es mayor al SDK nivel 23
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //Si no se aceptan los permisos
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }else {
+                //Almacenamos en un objeto de tipo Location la ultima posicion almacenada del GPS
+                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             }
-        });
+        }else{
+            //Almacenamos en un objeto de tipo Location la ultima posicion almacenada del GPS
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+
+        mostrarLocalizacion(location);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                mostrarLocalizacion(location);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0, locationListener);
+
+
+        /*
+        //-----------atos de los proveedores----------
+        //Almaceno en una lista todos los proveedores de servicios
+        List<String> listaProveedores = locationManager.getAllProviders();
+        //Almaceno en un objeto de la clase LocationProvider un determinado proveedor de la lista
+        LocationProvider proveedorUno = locationManager.getProvider(listaProveedores.get(0));
+        //Obtengo la presicion del proveedor selecconado
+        int presicioProveedor = proveedorUno.getAccuracy();
+        //Determino si el proveedor tiene soporte para altitud
+        boolean tieneAltitud = proveedorUno.supportsAltitude();
+
+        //Establesco criterios para buscar un proveedor
+        Criteria criteria = new Criteria();
+        //Establesco el tipo de afinidad el cual quiero que tenga el proveedor a buscar
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        //Guardo el mejor proveedor que haya concordado con el criterio puesto anteriormente
+        String mejorProveedor = locationManager.getBestProvider(criteria, true);
+        //----------------------------------------------
+        */
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }else {
+                locationManager.removeUpdates(locationListener);
+            }
+        }else{
+            locationManager.removeUpdates(locationListener);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0, locationListener);
+            }
+        }else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0, locationListener);
+        }
+    }
+
+    public void mostrarLocalizacion(Location loc) {
+        if (loc != null){
+            hiloConexion = new ObtenerWebService();
+            //Mandamos los parámetros que se nesecitan pasando la longitud y la latitud que trae el objeto de tipo Location
+            hiloConexion.execute(String.valueOf(loc.getLatitude()), String.valueOf(loc.getLongitude()));
+        }else{
+            Toast.makeText(this, "El objeto Location es null.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 
     public class ObtenerWebService extends AsyncTask<String, Integer, String>{
@@ -77,8 +208,10 @@ public class MainActivity extends AppCompatActivity {
             //Creamos la cadena con los parametros recibidos de latitud y longitud
             cadena = cadena + voids[0] + "," + voids[1] + "&sensor=false";
 
+            Log.i("CADENA", cadena);
+
             URL url = null;
-            String devuelve = "";
+            String devuelve = "Sin contenido.";
 
             try {
                 url = new URL(cadena);
@@ -110,7 +243,7 @@ public class MainActivity extends AppCompatActivity {
                     //En este caso obtenemos la primera direccion de los resultados
                     String direccion = "SIN DATOS PARA ESA LONGITUD Y LATITUD";
                     if (resultJSON.length()>0){
-                        direccion = resultJSON.getJSONObject(0).getString("formatted_addresss");
+                        direccion = resultJSON.getJSONObject(0).getString("formatted_address");
                     }
                     devuelve = "Direccion: " + direccion;
                 }
